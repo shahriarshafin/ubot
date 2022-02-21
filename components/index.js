cdcab3775e004752ac9b4584c43dba2a6a483d81
shafin <shafinshahriar340@gmail.com>
2022-02-22 02:24:50 +0600

    added bot as component

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as HeroSlider } from './HeroSlider';
 export { default as IsBot } from './IsBot';
 export { default as IsUser } from './IsUser';
 export { default as Nav } from './Nav';
+export { default as Ubot } from './Ubot';
